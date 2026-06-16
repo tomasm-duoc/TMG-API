44267167493d29458b54d64bbd4fbf9413ccb3ee
Tomas Martinez <to.martinezg@duocuc.cl>
2026-06-16 13:16:44 -0400

    Creacion de UsuarioInfo.java
    Objeto para poder almacenar los datos relevantes de los usuarios separado de las credenciales para autenticación

---
+++ api/src/main/java/tmg/ohiggins/api/Model/UsuarioInfo.java
@@ -0,0 +1,32 @@
+package tmg.ohiggins.api.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios_info")
+public class UsuarioInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+    @NotBlank
+    private String nombre;
+    @NotBlank
+    private String appaterno;
+    @NotBlank
+    private String apmaterno;
+    @NotBlank
+    private String rut;
+    @NotBlank
+    private Integer edad;
+}
