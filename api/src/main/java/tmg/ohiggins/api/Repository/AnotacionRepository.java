5402b183f599e9662dd6688d93f58141bce0e72c
Miguel González <migu.gonzalezc@duocuc.cl>
2026-06-30 16:26:13 -0400

    Update AnotacionRepository.java

--- api/src/main/java/tmg/ohiggins/api/Repository/AnotacionRepository.java
+++ api/src/main/java/tmg/ohiggins/api/Repository/AnotacionRepository.java
@@ -1,5 +1,10 @@
 package tmg.ohiggins.api.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AnotacionRepository {
+import tmg.ohiggins.api.Model.Anotacion;
+
+@Repository
+public interface AnotacionRepository extends JpaRepository<Anotacion, Integer> {
 
 }
